808f4426628e72c8ac32be3166f878701d41401a
eddigio <39969878+eddigio@users.noreply.github.com>
2020-11-14 06:28:46 -0500

    Agregar archivos de proyecto.

---
+++ Semana3Navegacion/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Semana3Navegacion
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrir_Venta_dos_Clicked(object sender, EventArgs e)
+        {
+            string user = txtuser.Text;
+            string pass = txtpass.Text;
+            string user_val = "uisrael";
+            string pass_val = "uisrael2020";
+            await Navigation.PushAsync(new Ventana_dos(user,pass));
+        }
+    }
+}
---
+++ Semana3Navegacion/Ventana_dos.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Semana3Navegacion
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Ventana_dos : ContentPage
+    {
+        public Ventana_dos(string user, string pass)
+        {
+            InitializeComponent();
+            lblUser.Text = user;
+            txtPass.Text = pass;
+        }
+    }
+}
